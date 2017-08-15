38d11d379c3b4c9f48f30b1044da26b1c08aeb49
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-15 19:05:05 +0200

    add manage.py file

--- bcpp_clinic_labs/settings.py
+++ bcpp_clinic_labs/settings.py
@@ -15,7 +15,7 @@ import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-APP_NAME = 'bcpp__clinic_labs'
+APP_NAME = 'bcpp_clinic_labs'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
