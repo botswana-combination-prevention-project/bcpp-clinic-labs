4ffa75144e815d302204ae9813c0c18d3398fba5
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-22 12:09:16 +0200

    fix import errors

--- bcpp_clinic_labs/model_mixins.py
+++ bcpp_clinic_labs/model_mixins.py
@@ -1,17 +1,16 @@
 from django.db import models
 
+from edc_consent.model_mixins import RequiresConsentMixin
 from edc_lab.model_mixins.requisition import RequisitionIdentifierMixin
 from edc_lab.model_mixins.requisition import RequisitionModelMixin, RequisitionStatusMixin
 from edc_map.site_mappers import site_mappers
 from edc_metadata.model_mixins.updates import UpdatesRequisitionMetadataModelMixin
 from edc_offstudy.model_mixins import OffstudyMixin
+from edc_reference.model_mixins import ReferenceModelMixin
 from edc_search.model_mixins import SearchSlugManager
 from edc_visit_tracking.managers import CrfModelManager as VisitTrackingCrfModelManager
 from edc_visit_tracking.model_mixins import CrfModelMixin as VisitTrackingCrfModelMixin
 from edc_visit_tracking.model_mixins import PreviousVisitModelMixin
-from edc_consent.model_mixins import RequiresConsentMixin
-from edc_metadata.model_mixins.rules.metadata_rules_model_mixin import MetadataRulesModelMixin
-from edc_metadata.rules.site import site_metadata_rules
 
 
 class Manager(VisitTrackingCrfModelManager, SearchSlugManager):
@@ -21,8 +20,8 @@ class Manager(VisitTrackingCrfModelManager, SearchSlugManager):
 class SubjectRequisitionModelMixin(
         RequisitionModelMixin, RequisitionStatusMixin, RequisitionIdentifierMixin,
         VisitTrackingCrfModelMixin, OffstudyMixin,
-        PreviousVisitModelMixin, RequiresConsentMixin,
-        UpdatesRequisitionMetadataModelMixin, MetadataRulesModelMixin, models.Model):
+        PreviousVisitModelMixin, RequiresConsentMixin, ReferenceModelMixin,
+        UpdatesRequisitionMetadataModelMixin, models.Model):
 
     objects = Manager()
 
@@ -40,16 +39,6 @@ class SubjectRequisitionModelMixin(
             'identifier_prefix']
         return fields
 
-    def run_metadata_rules(self):
-        """Runs the rule groups for this .
-
-        Gets called in the signal.
-        """
-        for rule_group in site_metadata_rules.registry.get(self._meta.rulegroup_app_label, []):
-            if rule_group._meta.source_model == self._meta.label_lower:
-                rule_group.evaluate_rules(visit=self)
-
     class Meta(VisitTrackingCrfModelMixin.Meta, RequiresConsentMixin.Meta):
         abstract = True
         consent_model = 'bcpp_clinic_subject.subjectconsent'
-        rulegroup_app_label = 'bcpp_clinic_metadata_rules'
